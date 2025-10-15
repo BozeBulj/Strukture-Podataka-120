bb2d69d949d2b0593bdde4a24305c16ee0c358ea
BozeBulj <abc>
2025-10-15 16:01:10 +0200

    Prvi zad. lab. vjezbi

---
+++ ZAD1/Source.cpp
@@ -0,0 +1,57 @@
+/*Napisati program koji prvo proèita koliko redaka ima datoteka, tj. koliko ima studenata
+zapisanih u datoteci. Nakon toga potrebno je dinamièki alocirati prostor za niz struktura
+studenata (ime, prezime, bodovi) i uèitati iz datoteke sve zapise. Na ekran ispisati ime,
+prezime, apsolutni i relativni broj bodova.
+Napomena: Svaki redak datoteke sadrži ime i prezime studenta, te broj bodova na kolokviju.
+relatvan_br_bodova = br_bodova/max_br_bodova*100*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	char ime[15];
+	char prezime[15];
+	int bod;
+}student;
+
+int ispis(student*, int);
+
+int main() {
+
+	int n=0;
+	char ime1[50];
+	char prez[50];
+	int bodovi;
+
+	FILE* f = fopen("nesto.txt", "r");
+	if (f == NULL) {
+		printf("GReska: file ne postoji");
+		return 1;
+	}
+
+	while (fscanf(f, "%s %s %d", ime1, prez, &bodovi) == 3) { //brojac redaka
+		n++;
+	}
+	rewind(f);
+
+	student* s = (student*)malloc(n * sizeof(student));
+
+	for (int i = 0; i < n; i++) {
+		fscanf(f, "%s %s %d", s[i].ime, s[i].prezime, &s[i].bod);
+	}
+
+	ispis(s, n);
+	free(s);
+	return 0;
+}
+
+int ispis(student *x,int k) {
+
+	int i;
+	for (i = 0; i < k; i++) {
+		printf("%s %s %d\n", x[i].ime, x[i].prezime, x[i].bod);
+	}
+	return 0;
+}
+	
+	
